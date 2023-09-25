754ab125f50b624722e6d839c70746bfa3ee3eaf
Frederik-DTU <s164222@student.dtu.dk>
2023-09-26 00:43:27 +0200

    HPC

---
+++ submit_score.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#BSUB -q gpuv100
+#BSUB -gpu "num=1"
+#BSUB -J R200
+#BSUB -n 4
+#BSUB -W 24:00
+#BSUB -R "rusage[mem=32GB]"
+#BSUB -u fmry@dtu.dk
+#BSUB -o models/output/output_%J.out
+#BSUB -e models/error/error_%J.err
+#BSUB -B
+#BSUB -N
+
+#Load the following in case
+module swap python3/3.9.11
+module swap cuda/8.0
+module swap cudnn/v7.0-prod-cuda8
+
+python3 train_score.py \
+    --model RN \
+    --order s2 \
+    --dim 200
