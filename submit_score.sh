cbe768f662686acddfb963801b31bc458d7bc797
Frederik-DTU <s164222@student.dtu.dk>
2024-02-05 17:13:25 +0100

    Update

--- submit_score.sh
+++ submit_score.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #BSUB -q gpuv100
-#BSUB -J Ellipsoid2_s1
+#BSUB -J S2_s1
 #BSUB -n 4
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 24:00
@@ -18,7 +18,7 @@ module swap cudnn/v8.9.1.23-prod-cuda-12.X
 module swap python3/3.10.12
 
 python3 train_score.py \
-    --manifold Ellipsoid \
+    --manifold SN \
     --dim 2 \
     --loss_type dsm \
     --load_model 0 \
