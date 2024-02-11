4053e9ff0723e0a5eff19e8ffd66fde5e1705d9b
Frederik-DTU <s164222@student.dtu.dk>
2024-02-11 11:35:20 +0100

    Update

--- submit_score.sh
+++ submit_score.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#BSUB -q gpua100
-#BSUB -J SPDN2_s2
+#BSUB -q gpuv100
+#BSUB -J Landmarks50_s2
 #BSUB -n 4
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 24:00
@@ -18,8 +18,8 @@ module swap cudnn/v8.9.1.23-prod-cuda-12.X
 module swap python3/3.10.12
 
 python3 train_score.py \
-    --manifold SPDN \
-    --dim 2 \
+    --manifold Landmarks \
+    --dim 50 \
     --loss_type dsm \
     --load_model 0 \
     --T_sample 0 \
