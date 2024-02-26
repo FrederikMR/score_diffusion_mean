01e92cc1b78b26d20cb09362d31258f6e0a74c09
Frederik-DTU <s164222@student.dtu.dk>
2024-02-26 14:33:30 +0100

    Updateø

--- submit_score.sh
+++ submit_score.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #BSUB -q gpuv100
-#BSUB -J Circle_s1T
+#BSUB -J Sym20_s1
 #BSUB -n 4
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 24:00
@@ -18,11 +18,11 @@ module swap cudnn/v8.9.1.23-prod-cuda-12.X
 module swap python3/3.10.12
 
 python3 train_score.py \
-    --manifold Circle \
-    --dim 1 \
+    --manifold Sym \
+    --dim 20 \
     --loss_type dsmvr \
     --load_model 0 \
-    --T_sample 1 \
+    --T_sample 0 \
     --t 0.1 \
     --train_net s1 \
     --max_T 1.0 \
