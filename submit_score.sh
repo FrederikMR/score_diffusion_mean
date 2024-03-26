d210a47e6aac1b34a30ef09ce629ada1fde7e16f
Frederik-DTU <s164222@student.dtu.dk>
2024-03-26 10:11:17 +0100

    Update

--- submit_score.sh
+++ submit_score.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #BSUB -q gpuv100
-#BSUB -J Sphere5_s2
+#BSUB -J HypParaboloid_s2
 #BSUB -n 4
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 24:00
@@ -18,9 +18,9 @@ module swap cudnn/v8.9.1.23-prod-cuda-12.X
 module swap python3/3.10.12
 
 python3 train_score.py \
-    --manifold Sphere \
-    --dim 5 \
-    --s1_loss_type dsm \
+    --manifold HypParaboloid \
+    --dim 2 \
+    --s1_loss_type dsmvr \
     --s2_loss_type dsmdiagvr \
     --load_model 0 \
     --T_sample 0 \
