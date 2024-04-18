b45c63af833a41de1f95637df99f7b734d74f01c
Frederik-DTU <s164222@student.dtu.dk>
2024-04-18 10:40:57 +0200

    Update

--- submit_score.sh
+++ submit_score.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #BSUB -q gpuv100
-#BSUB -J Sphere10_s1s2vr
+#BSUB -J Sphere2_s1s2vr
 #BSUB -n 4
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 24:00
@@ -19,11 +19,11 @@ module swap python3/3.10.12
 
 python3 train_score.py \
     --manifold Sphere \
-    --dim 10 \
+    --dim 2 \
     --train_net s1s2 \
     --s1_loss_type dsmvr \
     --s2_loss_type dsmvr \
-    --epochs 1000 \
+    --epochs 50000 \
     --lr_rate 0.0002 \
     --T 1.0 \
     --dt_steps 1000 \
