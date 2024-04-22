edb00b2edc1eba1c2da8cc3dd1b3ae061ea58c0a
Frederik Mbius Rygaard <fmry@pop-os.localdomain>
2024-04-22 22:59:54 +0200

    Update

--- submit_score.sh
+++ submit_score.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #BSUB -q gpuv100
-#BSUB -J Landmarks20_s1s2vr
+#BSUB -J Sphere3_s2vr
 #BSUB -n 4
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 24:00
 #BSUB -R "rusage[mem=32GB]"
 #BSUB -u fmry@dtu.dk
-#BSUB -o scores/output/hpc/output_%J.out
-#BSUB -e scores/error/hpc/error_%J.err
+#BSUB -o scores/output/output_%J.out
+#BSUB -e scores/error/error_%J.err
 #BSUB -B
 #BSUB -N
 
@@ -18,21 +18,21 @@ module swap cudnn/v8.9.1.23-prod-cuda-12.X
 module swap python3/3.10.12
 
 python3 train_score.py \
-    --manifold Landmarks \
-    --dim 20 \
-    --train_net s1s2 \
+    --manifold Sphere \
+    --dim 3 \
     --s1_loss_type dsmvr \
     --s2_loss_type dsmvr \
-    --epochs 100 \
+    --load_model 0 \
+    --T_sample 0 \
+    --t 0.01 \
+    --train_net s2 \
+    --max_T 5.0 \
     --lr_rate 0.0002 \
-    --T 1.0 \
-    --dt_steps 1000 \
+    --epochs 50000 \
     --x_samples 64 \
     --t_samples 256 \
     --repeats 16 \
-    --t0_sample 0 \
-    --t0 0.01 \
-    --gamma 1.0 \
-    --load_model 0 \
+    --samples_per_batch 32 \
+    --dt_steps 1000 \
     --save_step 100 \
-    --seed 2712 \
+    --seed 2712
