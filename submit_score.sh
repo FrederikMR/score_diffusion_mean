8efd8415fd6b6e053aa6fc7a86f5db4f2bc790c4
Frederik-DTU <s164222@student.dtu.dk>
2024-04-29 16:01:03 +0200

    Update

--- submit_score.sh
+++ submit_score.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #BSUB -q gpuv100
-#BSUB -J Sphere2_s1s2
+#BSUB -J Sphere3_s1s2
 #BSUB -n 4
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 24:00
@@ -19,7 +19,7 @@ module swap python3/3.10.12
 
 python3 train_score.py \
     --manifold Sphere \
-    --dim 2 \
+    --dim 3 \
     --s1_loss_type dsm \
     --s2_loss_type dsm \
     --load_model 0 \
