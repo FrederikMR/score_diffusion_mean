059c59fc3b91ea0f642d30a2df86c8c3278c5e47
Frederik-DTU <s164222@student.dtu.dk>
2024-05-06 13:04:57 +0200

    Update

--- submit_score.sh
+++ submit_score.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #BSUB -q gpuv100
-#BSUB -J Euclidean5_st
+#BSUB -J Euclidean5_s1dsm
 #BSUB -n 4
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 24:00
@@ -20,12 +20,12 @@ module swap python3/3.10.12
 python3 train_score.py \
     --manifold Euclidean \
     --dim 5 \
-    --s1_loss_type vsm \
+    --s1_loss_type dsm \
     --s2_loss_type dsm \
     --load_model 0 \
     --T_sample 0 \
     --t0 0.01 \
-    --train_net t \
+    --train_net s1 \
     --max_T 1.0 \
     --lr_rate 0.0002 \
     --epochs 50000 \
