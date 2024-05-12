8e9d15dac2e750c63bdce2d3e2ab3311f8084b42
Frederik-DTU <s164222@student.dtu.dk>
2024-05-12 20:27:18 +0200

    Update

--- submit_score.sh
+++ submit_score.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #BSUB -q gpuv100
-#BSUB -J Sphere50_s1T
+#BSUB -J Euclidean50_s1T
 #BSUB -n 4
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 24:00
@@ -18,19 +18,19 @@ module swap cudnn/v8.9.1.23-prod-cuda-12.X
 module swap python3/3.10.12
 
 python3 train_score.py \
-    --manifold Sphere \
+    --manifold Euclidean \
     --dim 50 \
     --s1_loss_type dsm \
     --s2_loss_type dsm \
     --load_model 0 \
     --T_sample 1 \
-    --t0 0.01 \
+    --t0 0.1 \
     --train_net s1 \
     --max_T 1.0 \
     --lr_rate 0.001 \
     --epochs 50000 \
     --warmup_epochs 1000 \
-    --x_samples 32 \
+    --x_samples 3200 \
     --t_samples 100 \
     --repeats 32 \
     --dt_steps 100 \
