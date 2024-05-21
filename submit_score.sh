ec679e7b1277ec224ee74bae3cf2682df5fd7ee2
Frederik-DTU <s164222@student.dtu.dk>
2024-05-21 23:30:47 +0200

    Update

--- submit_score.sh
+++ submit_score.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #BSUB -q gpuv100
-#BSUB -J Ellipsoid2_s1vsm
+#BSUB -J Ellipsoid10_s1vsm
 #BSUB -n 4
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 4:00
@@ -19,7 +19,7 @@ module swap python3/3.10.12
 
 python3 train_score.py \
     --manifold Ellipsoid \
-    --dim 2 \
+    --dim 10 \
     --s1_loss_type vsm \
     --s2_loss_type dsm \
     --load_model 0 \
